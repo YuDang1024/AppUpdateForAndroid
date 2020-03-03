3eec21328f0bcceae27c4714aa3f29dba2b2a57a
YuDang1024 <374489401@qq.com>
2020-03-03 17:39:29 +0800

    删除旧目录文件

--- app/src/main/java/com/kingsoft/appupdata/updata/net/KNetGetCallback.java
+++
@@ -1,8 +0,0 @@
-package com.kingsoft.appupdata.updata.net;
-
-public interface KNetGetCallback {
-    // get成功的话应该返回一个json字符串
-    void getSuccess(String reponse);
-    //get失败抛出异常
-    void getFailed(Throwable throeable);
-}
